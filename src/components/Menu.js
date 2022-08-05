e501cb911c8cee1df942461b4edf3a3ba68e1b2a
tomikode <tomi.piisila@gmail.com>
2022-08-06 09:56:37 +1000

    navbar fixed position, working on menu

---
+++ src/components/Menu.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Menu = () => {
+
+
+	return (
+		<div>
+			<ul>
+				<li>Home</li>
+				<li>Contact</li>
+				<li><img className="linkedIcon" src="linkedinIcon.png" />LinkedIn</li>
+                <li><img className="linkedIcon" src="githubIcon.png" />GitHub</li>
+			</ul>
+		</div>
+	);
+};
+
+export default Menu;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { MenuIcon } from "@heroicons/react/solid";
+import Menu from "./Menu";
 
 const Navbar = () => {
+
+	const showMenu = () => {
+		const body = document.querySelector("body")
+		body.classList.add("disableScroll")
+	};
+
 	return (
 		<div className="navbar">
-        <img src="tp.png" className="logo"/>
-				<MenuIcon className="icon" />
+			<div className="navbarFlex">
+				<img src="tp.png" className="logo" />
+				<MenuIcon onClick={showMenu} className="icon" />
+			</div>
+			<div className="menu">
+				<Menu />
+			</div>
 		</div>
 	);
 };
